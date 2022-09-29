ff293c66a400f9b411db154a00160648e333eb6c
Martha Milagros Vilchez Herrera (24161) <martha.vilchez@est.zamorano.edu>
2022-09-28 23:14:05 -0600

    Simon

---
+++ Lab04/ejercicio2.py
@@ -0,0 +1,21 @@
+maximo = 0 
+minimo = 0
+primer_numero = True
+while True: 
+    try:
+        lista = input("Ingrese un número: ")
+        if (lista == 'done'):
+            break
+        else:
+            if (primer_numero):
+                maximo = int(lista)
+                minimo = int(lista)
+                primer_numero = False 
+            else:
+                if (int(lista) > maximo): maximo = int(lista)
+                if (int(lista) <minimo): minimo = int(lista)
+    except: 
+        print("Valor no es válido")
+        continue
+print("Máximo", maximo)
+print("Mínimo", minimo)
---
+++ lab2/lab02-formatosdevariable.py
@@ -0,0 +1,13 @@
+# Tendencias e Innovación en Tecnologia Agricola (TEA)
+# Ejemplos de mejores practicas al nombrar variables (dos formatos)
+
+# Ejemplos de nombres de variables utilizando pothole_case_naming
+temp_celcius = 26.67
+primer_nombre = "Juan"
+
+
+# Ejemplos de nombres de variables utilizando camelCase
+clase = "TEA"
+tempFarenheit = 80.0
+primerNombre = "Juan"
+apellido = "White"
